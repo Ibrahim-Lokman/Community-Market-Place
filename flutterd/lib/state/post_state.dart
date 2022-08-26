8a1f5b91cd5bfdb5e16ac21140f45667af6ed1bf
Abdullah-Hasan-Sajjad <64843174+Abdullah-Hasan-Sajjad@users.noreply.github.com>
2022-08-26 16:09:17 +0600

    token updated in createcommunity state

--- flutterd/lib/state/post_state.dart
+++ flutterd/lib/state/post_state.dart
@@ -185,6 +185,7 @@ class PostState with ChangeNotifier {
 
   Future<bool> registerNewCommunity(String communityName) async {
     try {
+      var token = storage.getItem('token');
       String url = 'http://10.0.2.2:8000/api/categorys/';
       http.Response response = await http.post(
         url,
