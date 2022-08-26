e5639d835d64b2337ac36bf24b74539d1be9a06a
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2022-08-26 12:52:26 +0600

    Initial commit

---
+++ flutterd/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutterd/screens/category_screens.dart';
+import 'package:flutterd/screens/home_screens.dart';
+import 'package:flutterd/screens/login_screens.dart';
+import 'package:flutterd/screens/post_details_screens.dart';
+import 'package:flutterd/screens/register_screens.dart';
+import 'package:flutterd/state/post_state.dart';
+import 'package:localstorage/localstorage.dart';
+import 'package:provider/provider.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    LocalStorage storage = LocalStorage("usertoken");
+    return ChangeNotifierProvider(
+      create: (ctx) => PostState(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          accentColor: Colors.blueGrey,
+        ),
+        home: FutureBuilder(
+          future: storage.ready,
+          builder: (context, snapshop) {
+            if (snapshop.data == null) {
+              return Scaffold(
+                body: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            }
+            if (storage.getItem('token') == null) {
+              return LoginScreens();
+            }
+            return HomeScreens();
+          },
+        ),
+        routes: {
+          HomeScreens.routeName: (ctx) => HomeScreens(),
+          PostDetailsScreens.routeName: (ctx) => PostDetailsScreens(),
+          CategoryScreens.routeName: (ctx) => CategoryScreens(),
+          LoginScreens.routeName: (ctx) => LoginScreens(),
+          RegisterScreens.routeName: (ctx) => RegisterScreens(),
+        },
+      ),
+    );
+  }
+}
---
+++ flutterd/lib/widgets/app_drawer.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutterd/screens/login_screens.dart';
+import 'package:localstorage/localstorage.dart';
+
+
+class AddDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    LocalStorage storage = LocalStorage("usertoken");
+    _logoutnow() {
+      storage.clear();
+      Navigator.of(context).pushReplacementNamed(LoginScreens.routeName);
+    }
+
+
+
+    return Drawer(
+      child: Column(
+        children: [
+          Spacer(),
+          Text(
+            "About",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 28,
+              color: Colors.indigo,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Text(
+                "Best Community market place in the world. Version: 0.0.1"),
+          ),
+          
+          ListTile(
+            onTap: () {
+             // _creategroupnow();
+            },
+            trailing: Icon(
+              Icons.people,
+              color: Theme.of(context).accentColor,
+            ),
+	          title : Text('Create Group'),
+	          ),
+
+          ListTile(
+            onTap: () {
+             // _creategroupnow();
+            },
+            trailing: Icon(
+              Icons.input,
+              color: Theme.of(context).accentColor,
+            ),
+	          title : Text('Join Group'),
+	          ),
+
+
+          ListTile(
+            onTap: () {
+             // _creategroupnow();
+            },
+            trailing: Icon(
+              Icons.note_add,
+              color: Theme.of(context).accentColor,
+            ),
+	          title : Text('Create Post'),
+	          ),
+	      
+          Spacer(),
+          Divider(),
+          
+          ListTile(
+            onTap: () {
+              _logoutnow();
+            },
+            trailing: Icon(
+              Icons.logout,
+              color: Theme.of(context).accentColor,
+            ),
+            title: Text("LogOut"),
+          ),
+        ],
+      ),
+    );
+  }
+}
